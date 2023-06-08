5e10d9bd13f4998e6a63f11bd9486a5bbfc07796
Allan RENOU <renou.allan@live.fr>
2023-06-08 18:41:02 +0200

    Ajout route Axe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule, OnInit } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EncyclopedieComponent } from './encyclopedie/encyclopedie.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { AxesComponent } from './axes/axes.component';
 
 export const URL_PARAM_ID_PROJET = "idProj";
 export const URL_PARAM_ID_FICHE = "idFiche";
@@ -9,7 +10,8 @@ export const URL_PARAM_ID_FICHE = "idFiche";
 const routes: Routes = [
   { path : "", component : AccueilComponent },
   { path: ":" + URL_PARAM_ID_PROJET + "/encyclopedie", title:"Encyclopédie", component : EncyclopedieComponent },
-  { path: ":" + URL_PARAM_ID_PROJET + "/encyclopedie/:" + URL_PARAM_ID_FICHE, title:"Encyclopédie", component : EncyclopedieComponent }
+  { path: ":" + URL_PARAM_ID_PROJET + "/encyclopedie/:" + URL_PARAM_ID_FICHE, title:"Encyclopédie", component : EncyclopedieComponent },
+  { path: ":" + URL_PARAM_ID_PROJET + "/axes", title:"Axes", component : AxesComponent }
 ];
 
 @NgModule({
